5f685a4cdbb74c6f59e9096569f4c6705500c5e5
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-25 16:09:11 +0530

    redux added to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// import redux utilities
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+// importing files
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import chat from './reducers'
+
+// creating a store for redux
+const store = createStore(chat)
+
+// creating app for rendering
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+// rendering react-redux app in DOM
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
