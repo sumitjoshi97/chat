a05952d85f0f6a04cb1585dc8506bd690266631f
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-25 17:10:01 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React, { Component } from 'react';
-import {Sidebar} from './containers/Sidebar'
-import {MessagesList} from './containers/MessagesList'
-import {AddMessage} from './containers/AddMessage'
+import {Sidebar} from './containers/Sidebar/Sidebar'
+import {MessagesList} from './containers/MessagesList/MessagesList'
+import {AddMessage} from './containers/AddMessage/AddMessage'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div id="container">
-                <aside id="sidebar">Users</aside>
                 <Sidebar />
                 <section id="main">
                     <MessagesList />
                     <AddMessage />
-                    <section id="messages-list">Messages list</section>
-                    <section id="new-message">New message</section>
                 </section>        
             </div>        
         );
