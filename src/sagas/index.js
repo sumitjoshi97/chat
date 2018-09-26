abb4ae1801b0fa950b1caabe4c7428e64559bfb8
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-26 11:17:26 +0530

    refactored

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,7 +3,7 @@ import * as actionTypes from '../store/actions/actionTypes'
 
 const handleNewMessage = function* handleNewMessage(params) {
     yield takeEvery(actionTypes.ADD_MESSAGE, action => {
-        action.author = params.username,
+        action.author = params.username
         params.socket.send(JSON.stringify(action))
     })
 }
