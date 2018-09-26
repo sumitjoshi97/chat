00c5933ca3b0ff51985367a6c97576c27dafc3ec
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-09-26 11:02:05 +0530

    added saga to redux middleware

--- src/index.js
+++ src/index.js
@@ -3,18 +3,43 @@ import ReactDOM from 'react-dom';
 
 // import redux utilities
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 
 // importing files
+import {addUser} from './store/actions/message'
 import './index.css';
 import App from './App';
+import handleNewMessage from './sagas'
+import username from './utils/name'
+
+// importing sockets
+import setupSocket from './sockets/index'
+
+// importing service worker 
 import registerServiceWorker from './registerServiceWorker';
 
-import chat from './reducers'
+import rootReducer from './store/reducers'
+import createSagaMiddleware from 'redux-saga';
+
+// create middleware for redux-saga
+const sagaMiddleWare = createSagaMiddleware()
+
+// composing redux dev tools 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // creating a store for redux
-const store = createStore(chat)
+const store = createStore(
+    rootReducer, 
+    composeEnhancers(applyMiddleware(sagaMiddleWare))
+)
 
+// initialize socket
+const socket = setupSocket(store.dispatch, username)
+
+// run socket in sagaMiddleware
+sagaMiddleWare.run(handleNewMessage, {socket, username})
+
+store.dispatch(addUser(username))
 // creating app for rendering
 const app = (
     <Provider store={store}>
